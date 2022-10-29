8c3d69a349ab2151a9832e996da21b6ff1e2bda4
Omprakash Choudhary <omseervi098@gmail.com>
2022-10-29 06:20:21 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("process.env.MONGODB_URL");
+mongoose.connect("mongodb://localhost:27017");
 const db = mongoose.connection;
 db.once("error", (error) => console.log("Error Connecting to Database"));
 db.once("open", () => console.log("Connected to Database"));
