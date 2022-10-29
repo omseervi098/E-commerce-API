b9c77c72b5e0ade818bf1d3145509d88a0fe371d
Omprakash Choudhary <omseervi098@gmail.com>
2022-10-29 06:14:14 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const { error } = require("console");
-const config = dotenv.config();
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect("process.env.MONGODB_URL");
 const db = mongoose.connection;
 db.once("error", (error) => console.log("Error Connecting to Database"));
 db.once("open", () => console.log("Connected to Database"));
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const db = require("./config/mongoose");
-const config = require("dotenv").config(); //to hide secret key
 app.use(express.urlencoded({ extended: true }));
 app.use("/", require("./routes"));
 const port = 3000;
