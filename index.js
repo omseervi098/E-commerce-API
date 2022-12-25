dd17a3feb12bd2b7bf152311e64fb720bd17e6a3
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-25 09:52:42 +0530

    update

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const db = require("./config/mongoose");
 const config = require("dotenv").config(); //to hide secret key
 app.use(express.urlencoded({ extended: true }));
 app.use("/", require("./routes"));
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.use("/products", require("./products"));
+router.get("/", (req, res) => {
+  res.send("Api is Up and Running");
+});
 module.exports = router;
