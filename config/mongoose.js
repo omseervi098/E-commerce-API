b8c3fb72f6da2011cb476ee069726159c464318f
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-25 09:49:39 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,13 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const config = dotenv.config();
-mongoose.connect(process.env.MONGODB_URL);
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URL;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(url, connectionParams);
 const db = mongoose.connection;
 db.once("error", (error) => console.log("Error Connecting to Database"));
 db.once("open", () => console.log("Connected to Database"));
