62ff02b34801d7412f511e3cf2a6cf358f2a6c6d
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-25 17:06:27 +0530

    added some checks

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -40,10 +40,18 @@ module.exports.create = async function (req, res) {
 //to delete a product
 module.exports.delete = async function (req, res) {
   try {
-    await Product.deleteOne({ id: req.params.id }); //delete product whose id matches
-    return res.status(200).json({
-      message: "Product deleted Successfully",
-    });
+    const data = await Product.deleteOne({ id: req.params.id }); //delete product whose id matches
+    if (data.deletedCount) {
+      //if product is deleted then return success message
+      return res.status(200).json({
+        message: "Product deleted Successfully",
+      });
+    } else {
+      //else return error message
+      return res.status(500).json({
+        message: "Product not found",
+      });
+    }
   } catch (e) {
     return res.status(500).json({
       message: "Error in deleting product",
@@ -54,14 +62,33 @@ module.exports.delete = async function (req, res) {
 module.exports.update = async function (req, res) {
   try {
     let product = await Product.findOne({ id: req.params.id }); //find by id
-    product.quantity += parseInt(req.query.number); //updating quantity
-    await product.save();
-    return res.status(200).json({
-      message: "Product updated Successfully",
-      data: {
-        product: product,
-      },
-    });
+    if (!product) {
+      return res.status(500).json({
+        message: "Product not found",
+      });
+    }
+    if (product.quantity + parseInt(req.query.number) < 0) {
+      //Check if quantity is negative
+      return res.status(500).json({
+        message: "Quantity cannot be negative",
+      });
+    }
+    if (product.quantity + parseInt(req.query.number) == 0) {
+      //delete product if quantity is 0
+      const data = await Product.deleteOne({ id: req.params.id });
+      return res.status(200).json({
+        message: "Product deleted Successfully",
+      });
+    } else {
+      product.quantity += parseInt(req.query.number); //updating quantity
+      await product.save();
+      return res.status(200).json({
+        message: "Product updated Successfully",
+        data: {
+          product: product,
+        },
+      });
+    }
   } catch (e) {
     return res.status(500).json({
       message: "Error in Updating quantity of product",
